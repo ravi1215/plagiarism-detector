6fa0700a03efd416d930b64fbea3652524648550
Ravi Pandey <118262813+ravi1215@users.noreply.github.com>
2024-12-29 20:48:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def detect_plagiarism():
     return render_template('index.html', result=detection_result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
